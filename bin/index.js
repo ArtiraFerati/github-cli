da30a0ca745b3b6d45480ce3f04d38f5561db14a
ArtiraFerati <artiraferati@gmail.com>
2021-09-07 21:08:34 +0200

    Added FileService class

--- bin/index.js
+++ bin/index.js
@@ -2,21 +2,47 @@
 
 const program = require('commander');
 
-const print = require('../lib/print');
-const printVersion = require('../lib/printVersion');
+const print = require('./print');
+const printVersion = require('./printVersion');
+var fs = require('fs');
+
+class FileService {
+    writeStringFile(path, content){
+        fs.writeFile(path, content, function(err){
+            if(err) throw err;
+            console.log('File is created successfully');
+        })
+
+    };
+
+    readStringFile(path){
+        var data = fs.readFileSync(path, 'utf-8');
+        console.log(data.toString());
+    };
+
+    exists(path){
+        if (fs.existsSync(path)){
+            console.log('Exists');
+        }
+        else console.log('Doesn\'t exist');
+    };
+}
+
+const newfile = new FileService();
 
 program
-    .command('gh-credits')
+    .command('credits')
     .description('Displays github-cli credits')
     .action(function(){
         print();
     })
 
 program
-    .command('gh-version')
+    .command('version')
     .description('Displays current version of the CLI')
     .action(function(){
         printVersion();
     })
 
 program.parse(process.argv);
+
