711301b9fe413122201fc316c70bbc8d9e62bb6f
ArtiraFerati <artiraferati@gmail.com>
2021-10-09 22:14:39 +0200

    feature/CLI-12 Fixed code with eslint rules

--- bin/index.js
+++ bin/index.js
@@ -1,35 +1,37 @@
 #!/usr/bin/env node
+/* eslint linebreak-style: ["error", "windows"] */
 
 const program = require('commander');
 const print = require('./print');
 const printVersion = require('./print-version');
 const ConfigurationService = require('./configuration-service');
-var configuration = new ConfigurationService();
+
+const configuration = new ConfigurationService();
 const getUser = require('./user-service');
 
 program
-    .command('credits')
-    .description('Displays github-cli credits')
-    .action(() => print.displayCredits())
+  .command('credits')
+  .description('Displays github-cli credits')
+  .action(() => print.displayCredits());
 
 program
-    .command('version')
-    .description('Displays current version of the CLI')
-    .action(() => printVersion.version())
+  .command('version')
+  .description('Displays current version of the CLI')
+  .action(() => printVersion.version());
 
 program
-    .command('create')
-    .description('Create configuration')
-    .argument('<username>', 'username')
-    .argument('<token>', 'token')
-    .action((username, token) => {
-        configuration.createConfiguration(username, token);
-    })
+  .command('create')
+  .description('Create configuration')
+  .argument('<username>', 'username')
+  .argument('<token>', 'token')
+  .action((username, token) => {
+    configuration.createConfiguration(username, token);
+  });
 
 program
-    .command('username')
-    .description('Get user info')
-    .argument('username' , 'user')
-    .action(user => getUser.getUserByUsername(user))
+  .command('username')
+  .description('Get user info')
+  .argument('username', 'user')
+  .action((user) => getUser.getUserByUsername(user));
 
 program.parse(process.argv);
