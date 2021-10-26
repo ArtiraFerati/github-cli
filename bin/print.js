4b1a4b0326fb7c35d37d08fac96835a74e9bbc19
Artira Ferati <artiraferati@gmail.com>
2021-10-26 21:22:03 +0200

    Update print.js

--- bin/print.js
+++ bin/print.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 
 const displayCredits = () => {
     console.log(chalk.green("=============================\n"));
-    console.log(chalk.green("GITHUB CLI \nProvided by Drixy Studios"));
+    console.log(chalk.green("GITHUB CLI \nProvided by Artira Ferati"));
     console.log(chalk.green("\n============================="));
 }
 
